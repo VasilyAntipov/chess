95f82b24368cc740e839f1e2571aaeb9e2773792
Vasily <ckpyt@inbox.ru>
2021-04-16 13:07:53 +0800

    пересобрал всю систему координат, полностью переработал метод создания фигур и положения их на доске. Теперь есть две системы,  браузерная и шахматная, из первой функцией вытекает вторая

--- client/src/play.js
+++ client/src/play.js
@@ -2,31 +2,36 @@ const newGame = document.getElementById('new-game');
 const deleteFigure = document.getElementById('delete-figure');
 const board = document.getElementById('chessboard-blank');
 const svgLayot = document.querySelector('.svg-layot');
+const layot = document.getElementById('chessboard-layot');
+const arrayWords = ['a', 'b', 'c', 'd', 'e', 'f', 'j', 'h'];
 const kef = 60;
-let selectedValue;
-
-
+let gameColor;
 
 newGame.addEventListener('click', function (event) {
   clearAll();
-  let checkBox = document.querySelectorAll('input[name="choice"]');
 
-  for (const rb of checkBox) {
-    if (rb.checked) {
-      selectedValue = rb.value;
-      break;
+  checkColorGame();
+  function checkColorGame() {
+    const checkBox = document.querySelectorAll('input[name="choice"]');
+    let selectValue;
+
+    for (const rb of checkBox) {
+      if (rb.checked) {
+        selectValue = rb.value;
+        break;
+      }
     }
+    if (selectValue == 'white') gameColor = { top: 'black', bot: 'white' };
+    else gameColor = { top: 'white', bot: 'black' };
   }
 
-  generateCoordinate(selectedValue);
-  generateFigures(selectedValue);
-});
+  generateCoordinate();
+  generateFigures();
+}); //++
 
-function generateCoordinate(color) {
-  const layot = document.getElementById('chessboard-layot');
+function generateCoordinate() {
   const xmlns = 'http://www.w3.org/2000/svg';
   const kef = 12.5;
-  const arrayWords = ['a', 'b', 'c', 'd', 'e', 'f', 'j', 'h'];
 
   let svg = document.createElementNS(xmlns, 'svg');
   svg.id = 'svg';
@@ -43,7 +48,7 @@ function generateCoordinate(color) {
         case 0:
           text.setAttributeNS(null, 'x', '2');
           text.setAttributeNS(null, 'y', 3.5 + kef * i);
-          switch (color) {
+          switch (gameColor.bot) {
             case 'black':
               text.textContent = i + 1;
               break;
@@ -54,7 +59,7 @@ function generateCoordinate(color) {
         case 1:
           text.setAttributeNS(null, 'x', 10.35 + kef * i);
           text.setAttributeNS(null, 'y', '99.25');
-          switch (color) {
+          switch (gameColor.bot) {
             case 'black':
               text.textContent = arrayWords[7 - i];
               break;
@@ -68,12 +73,12 @@ function generateCoordinate(color) {
   }
 
   layot.appendChild(svg);
-}
+} //++
 
-function generateFigures(color) {
+function generateFigures() {
   const pawn = {
     name: 'p',
-    move: ['p', 'one'],
+    move: ['p'],
   };
   const rook = {
     name: 'r',
@@ -95,61 +100,76 @@ function generateFigures(color) {
     name: 'q',
     move: ['f', 'b', 's', 'd'],
   };
-
+  const arrayFigures = [
+    rook,
+    knight,
+    bishop,
+    queen,
+    king,
+    bishop,
+    knight,
+    rook,
+  ];
+  const arrayPawns = [pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn];
   const arrayChess = [
-    [rook, pawn, , , , , pawn, rook],
-    [knight, pawn, , , , , pawn, knight],
-    [bishop, pawn, , , , , pawn, bishop],
-    [queen, pawn, , , , , pawn, queen],
-    [king, pawn, , , , , pawn, king],
-    [bishop, pawn, , , , , pawn, bishop],
-    [knight, pawn, , , , , pawn, knight],
-    [rook, pawn, , , , , pawn, rook],
+    arrayFigures,
+    arrayPawns,
+    [],
+    [],
+    [],
+    [],
+    arrayPawns,
+    arrayFigures,
   ];
 
-  for (let i = 0; i < 8; i++) {
-    for (let j = 0; j < 8; j++) {
-      if (arrayChess[i][j]) {
-        let figure = new Figure(i + 1, j + 1, arrayChess[i][j]);
-        figure.addFigure(color);
-      }
+  if (gameColor.bot == 'black') {
+    arrayFigures.reverse();
+  }
+
+  for (let y = 0; y < 8; y++) {
+    for (let x = 0; x < 8; x++) {
+      let figure = new Figure(x, y, arrayChess[y][x]);
+      figure.addFigure();
     }
+    if (y == 1) y = 5;
   }
 }
 
 class Figure {
-  constructor(x, y, obj) {
-    this.x = x;
-    this.y = y;
-    this.elemDiv = document.createElement('div');
-    let name;
+  constructor(coorX, coorY, obj) {
+    this.coorX = coorX;
+    this.coorY = coorY;
 
-    if (this.y < 3) {
+    this.elemDiv = document.createElement('div');
+    let name, color;
+
+    let [x, y] = CalcChessCoor(coorX, coorY);
+
+    if (y < 3) {
+      color = 'white';
       name = 'w' + obj.name;
     }
-    if (this.y > 6) {
+    if (y > 6) {
+      color = 'black';
       name = 'b' + obj.name;
     }
 
-    let color = selectedValue;
-
-    this.addFigure = function (color) {
-      const board = document.getElementById('chessboard-blank');
-
+    this.addFigure = function () {
       this.elemDiv.className = `piece ${name} square${x}${y}`;
 
       this.elemDiv.style.backgroundImage = `url('/chess/client/img/figures/${name}.png')`;
       this.elemDiv.style.transform = `
-      matrix(1, 0, 0, 1, 
-      ${getCoordinates(x, y, color).x},
-      ${getCoordinates(x, y, color).y}
-    `;
+        matrix(1, 0, 0, 1,
+        ${coorX * kef},
+        ${coorY * kef}
+      `;
       this.elemDiv.id = name + x + y;
       board.appendChild(this.elemDiv);
     };
 
     const ball = this.elemDiv;
-    let Cx, Cy;
+
+    let mouseX, mouseY;
 
     ball.onmousedown = function (event) {
       document.addEventListener('mousemove', onMouseMove);
@@ -160,51 +180,39 @@ class Figure {
 
       function moveAt(pageX, pageY) {
         const rect = board.getBoundingClientRect();
-        Cx =
-          pageX -
-          rect.x -
-          getCoordinates(x, y, color, kef).x -
-          ball.offsetWidth / 2;
-        Cy =
-          pageY -
-          rect.y -
-          getCoordinates(x, y, color, kef).y -
-          ball.offsetHeight / 2;
 
-        ball.style.left = Cx + 'px';
-        ball.style.top = Cy + 'px';
+        mouseX = pageX - rect.x - coorX * kef - ball.offsetWidth / 2;
+        mouseY = pageY - rect.y - coorY * kef - ball.offsetHeight / 2;
+
+        ball.style.left = mouseX + 'px';
+        ball.style.top = mouseY + 'px';
       }
 
       board.appendChild(ball);
+
       moveAt(event.pageX, event.pageY);
 
       ball.style.cursor = 'grabbing';
       ball.onmouseup = function () {
         let flag;
-        let xDif = Math.round(Cx / kef);
-        let yDif = Math.round(Cy / kef);
 
-        let newMatrixX = getCoordinates(x, y, color).x + xDif * kef;
-        let newMatrixY = getCoordinates(x, y, color).y + yDif * kef;
+        let xDif = Math.round(mouseX / kef);
+        let yDif = Math.round(mouseY / kef);
 
-        let newX, newY;
+        let newCoorX = coorX + xDif;
+        let newCoorY = coorY + yDif;
 
-        if (color == 'white') {
-          newX = x + xDif;
-          newY = y - yDif;
-        } else if (color == 'black') {
-          newX = x - xDif;
-          newY = y + yDif;
-        }
+        let [newX, newY] = CalcChessCoor(newCoorX, newCoorY);
 
-        clickFigure();
-//////////////////////////не работает
+        // clickFigure();
+        //////////////////////////не работает
+
         function clickFigure() {
-          if (x == newX && y == newY) {
+          if (x == newCoorX && y == newCoorY) {
             svgLayot.innerHTML = `<rect class="rect"
-            transform = "matrix(1, 0, 0, 1, ${getCoordinates(x, y, color).x},${
-              getCoordinates(x, y, color).y
-            })"/>`;
+            transform = "matrix(1, 0, 0, 1, ${
+              getCoordinates(x, y, gameColor.bot).x
+            },${getCoordinates(x, y, gameColor.bot).y})"/>`;
           }
 
           board.addEventListener('click', function (event) {
@@ -215,12 +223,10 @@ class Figure {
             const clickX = Math.floor((pageX - layot.x) / kef);
             const clickY = Math.floor((pageY - layot.y) / kef);
 
-            alert()
             //animation(ball, clickX, clickY);
-            
           });
 
-          function animation(elem, x,y) {
+          function animation(elem, x, y) {
             // let start = Date.now(); // запомнить время начала
 
             let timer = setInterval(function () {
@@ -244,84 +250,75 @@ class Figure {
           }
         }
 
-        if (canMove() == 'can') {
-          putFigure(newX, newY, newMatrixX, newMatrixY);
-        } else {
-          backFigure();
+        function updateCoordinates() {
+          x = newX;
+          y = newY;
+          coorX = newCoorX;
+          coorY = newCoorY;
         }
 
-        checkTarget();
+        if (canMove() == 'can' && checkAndRemoveTarget() == 'check') {
+          putFigure(newCoorX, newCoorY);
+          updateCoordinates();
+        } else {
+          putFigure(coorX, coorY);
+          flag = 'stopCheckTarget';
+        }
 
         document.removeEventListener('mousemove', onMouseMove);
         ball.onmouseup = null;
 
-        function checkTarget() {
-          if (flag == 'stopCheckTarget') return;
-
+        function checkAndRemoveTarget() {
           let target = document.querySelector(`.square${newX}${newY}`);
-
-          if (
-            colorFigure(target.classList[1]) != colorFigure(ball.classList[1])
-          ) {
-            target.remove();
-            x = newX;
-            y = newY;
-          } else {
+          if (target) {
             if (target.id != ball.id) {
-              backFigure();
-            } else {
-              x = newX;
-              y = newY;
+              if (
+                target.classList[1].slice(0, 1) != ball.classList[1].slice(0, 1)
+              ) {
+                target.remove();
+                return 'check';
+              }
             }
-          }
+          } else return 'check';
         }
 
-        function backFigure() {
-          putFigure(
-            x,
-            y,
-            getCoordinates(x, y, color).x,
-            getCoordinates(x, y, color).y
-          );
-          flag = 'stopCheckTarget';
-        }
-
-        function putFigure(x, y, matrixX, matrixY) {
+        function putFigure(a, b) {
           ball.style.left = 0;
           ball.style.top = 0;
           ball.style.transform = `
             matrix(1, 0, 0, 1, 
-            ${matrixX},
-            ${matrixY})
+            ${a * kef},
+            ${b * kef})
           `;
-          ball.className = `piece ${name} square${x}${y}`;
-        }
-
-        function colorFigure(name) {
-          if (name.slice(0, 1) == 'w') return 'white';
-          else return 'black';
+          ball.className = `piece ${name} square${CalcChessCoor(a, b)[0]}${
+            CalcChessCoor(a, b)[1]
+          }`;
+          ball.id = name + CalcChessCoor(a, b)[0] + CalcChessCoor(a, b)[1];
         }
 
         function canMove() {
           let S;
-          if (ball.classList[1].slice(0, 1) == 'w') S = 1;
-          if (ball.classList[1].slice(0, 1) == 'b') S = -1;
+          let chessDifX = x - newX;
+          let chessDifY = y - newY;
+
+          if (color == 'white') S = 1;
+          if (color == 'black') S = -1;
 
           if (obj.move.includes('p')) {
             let target = document.querySelector(`.square${newX}${newY}`);
 
-            if (yDif * S == -1 && xDif == 0) {
+            if (chessDifY * S == -1 && chessDifX == 0) {
               if (target) return;
               return 'can';
             }
 
-            if ((y == 2 && yDif == -2) || (y == 7 && yDif == 2)) {
-              if (target || document.querySelector(`.square${newX}${newY - S}`))
+            if ((y == 2 && chessDifY == -2) || (y == 7 && chessDifY == 2)) {
+              if (target || document.querySelector(`.square${newX}${newY}`))
                 return;
               return 'can';
             }
 
-            if (yDif * S == -1 && Math.abs(xDif) == 1) {
+            if (chessDifY * S == -1 && Math.abs(chessDifX) == 1) {
               if (target) {
                 return 'can';
               }
@@ -343,36 +340,36 @@ class Figure {
 
           if (
             obj.move.includes('one') &&
-            (Math.abs(xDif) > 1 || Math.abs(yDif) > 1)
+            (Math.abs(chessDifX) > 1 || Math.abs(chessDifY) > 1)
           ) {
             return;
           }
 
-          if (obj.move.includes('f') && xDif == 0 && yDif < 0) {
+          if (obj.move.includes('f') && chessDifX == 0 && chessDifY * S < 0) {
             return 'can';
           }
 
-          if (obj.move.includes('b') && xDif == 0 && yDif > 0) {
+          if (obj.move.includes('b') && chessDifX == 0 && chessDifY * S > 0) {
             return 'can';
           }
 
-          if (obj.move.includes('s') && yDif == 0 && xDif != 0) {
+          if (obj.move.includes('s') && chessDifY == 0 && chessDifX != 0) {
             return 'can';
           }
 
           if (
             obj.move.includes('d') &&
-            xDif != 0 &&
-            Math.abs(xDif) == Math.abs(yDif)
+            chessDifX != 0 &&
+            Math.abs(chessDifX) == Math.abs(chessDifY)
           ) {
             return 'can';
           }
 
           if (
             obj.move.includes('h') &&
-            yDif != 0 &&
-            xDif != 0 &&
-            Math.abs(xDif) + Math.abs(yDif) == 3
+            chessDifY != 0 &&
+            chessDifX != 0 &&
+            Math.abs(chessDifX) + Math.abs(chessDifY) == 3
           ) {
             return 'can';
           }
@@ -386,22 +383,18 @@ class Figure {
   }
 }
 
-function getCoordinates(x, y, color) {
-  let strX, strY;
-  switch (color) {
-    case 'white':
-      strX = (x - 1) * kef;
-      strY = (8 - y) * kef;
-      break;
-    case 'black':
-      strX = (8 - x) * kef;
-      strY = (y - 1) * kef;
-      break;
+function CalcChessCoor(a, b) {
+  if (gameColor.bot == 'white') {
+    return [a + 1, 8 - b];
+  } else {
+    return [8 - a, b + 1];
   }
-  return { x: strX, y: strY };
 }
 
 function clearAll() {
-  let texts = document.getElementsByClassName('text');
+  let svg = document.getElementById('svg');
+  if (svg) {
+    svg.remove();
+  }
   board.innerHTML = '';
 }
